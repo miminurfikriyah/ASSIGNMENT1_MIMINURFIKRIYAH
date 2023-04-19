f204dacc714a7e424884badd4c0ce89076b94632
miminurfikriyah <miminurfikriyah@gmail.com>
2023-04-19 15:11:51 +0000

    changing my code

--- codingassign.c
+++ codingassign.c
@@ -7,15 +7,15 @@
 #define MAX_CHILD 5
 #define MSG_BUF_SIZE 100
 
-int num_child = 0;
-pid_t child_pids[MAX_CHILD];    //array to store child's PID
-int pipe_fds[MAX_CHILD][2];     //2D array to store file descriptors of the pipes
+int number_child = 0;
+pid_t child_pids[MAX_CHILD];    //array to store the child's PID
+int pipe_fds[MAX_CHILD][2];     //declaring 2D array to store file descriptors of the pipes
 
 sem_t sem;
 
 //when signal CTRL+C is received, it prints message and terminate the program
 void sigint_handler(int sig) {
-    printf("\nThere's an interruption. Exiting.\n");
+    printf("\nThere is some interruption. Exiting to the main page.\n");
     sem_destroy(&sem);
     exit(0);
 }
@@ -25,10 +25,10 @@ void sigint_handler(int sig) {
 void parent_process() {
     sleep(1);
     fflush(stdin);
-    printf("\nEnter message to send to children: ");
+    printf("\nEnter your  message to send to the children: ");
     char message[256];
     if( fgets(message, sizeof(message), stdin) == NULL){
-        printf("\nError reading input.");
+        printf("\nError reading the  input.");
         exit(1);}
 
     sem_wait(&sem);
@@ -57,7 +57,7 @@ int main() {
 
     for (int i = 0; i < MAX_CHILD; i++) {
         if (pipe(pipe_fds[i]) == -1) {
-            printf("Error creating pipe for child %d\n", i+1);
+            printf("Error creating the pipe for the child %d\n", i+1);
             exit(1);
         }
         pid_t pid = fork();
@@ -69,7 +69,7 @@ int main() {
             exit(0);
         } else {
             child_pids[i] = pid;
-            num_child++;
+            number_child++;
             if (i == MAX_CHILD-1) {
                 parent_process();
             }
